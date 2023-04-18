071fa8cc43f4877f5657f70870aba0c503ff4eee
Patrick Rudigier <patrick.rudigier@gantner.com>
2023-04-18 17:11:04 +0200

    removed deprecated styelint rules and added stylelint config to dev container

--- frontend/.devcontainer/init-container.sh
+++ frontend/.devcontainer/init-container.sh
@@ -5,10 +5,10 @@ export NG_CLI_ANALYTICS="ci"
 npm i -g @angular/cli@12
 
 # install code formatting and linting tools into workspace
-npm i eslint eslint-config-prettier eslint-plugin-prettier prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser typescript --save-dev
+npm i eslint eslint-config-prettier eslint-plugin-prettier prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser typescript htmlhint stylelint stylelint-prettier stylelint-config-recommended --save-dev
 
 # downlaod code formatting and linting config files
-#touch $WORKSPACE_FOLDER/.eslintrc
 curl -s "https://raw.githubusercontent.com/Gantner-Electronics/coding-guidelines/feature/devcontainers/frontend/guidelines/ecma/eslintrc.json" > $WORKSPACE_FOLDER/.eslintrc.json
-#touch $WORKSPACE_FOLDER/.prettierrc.json
 curl -s "https://raw.githubusercontent.com/Gantner-Electronics/coding-guidelines/feature/devcontainers/frontend/guidelines/ecma/prettier.json" > $WORKSPACE_FOLDER/.prettierrc.json
+curl -s "https://raw.githubusercontent.com/Gantner-Electronics/coding-guidelines/feature/devcontainers/frontend/guidelines/html/htmlhintrc.json" > $WORKSPACE_FOLDER/.htmlhintrc
+curl -s "https://raw.githubusercontent.com/Gantner-Electronics/coding-guidelines/feature/devcontainers/frontend/guidelines/style/stylelintrc.json" > $WORKSPACE_FOLDER/.stylelintrc.json
