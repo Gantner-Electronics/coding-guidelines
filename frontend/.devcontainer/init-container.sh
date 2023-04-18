d7943cb9d5e554ddce406c4b03092dc2a62ba545
Patrick Rudigier <patrick.rudigier@gantner.com>
2023-04-18 16:11:26 +0200

    changed config files name

--- frontend/.devcontainer/init-container.sh
+++ frontend/.devcontainer/init-container.sh
@@ -9,6 +9,6 @@ npm i eslint eslint-config-prettier eslint-plugin-prettier prettier @typescript-
 
 # downlaod code formatting and linting config files
 #touch $WORKSPACE_FOLDER/.eslintrc
-curl -s "https://raw.githubusercontent.com/Gantner-Electronics/coding-guidelines/feature/devcontainers/frontend/guidelines/ecma/eslintrc.json" > $WORKSPACE_FOLDER/.eslintrc
+curl -s "https://raw.githubusercontent.com/Gantner-Electronics/coding-guidelines/feature/devcontainers/frontend/guidelines/ecma/eslintrc.json" > $WORKSPACE_FOLDER/.eslintrc.json
 #touch $WORKSPACE_FOLDER/.prettierrc.json
 curl -s "https://raw.githubusercontent.com/Gantner-Electronics/coding-guidelines/feature/devcontainers/frontend/guidelines/ecma/prettier.json" > $WORKSPACE_FOLDER/.prettierrc.json
