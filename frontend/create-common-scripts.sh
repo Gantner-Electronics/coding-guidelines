4fceb6165bd162c93a4f5d5d3f0e2e4063e2fa10
Patrick Rudigier <patrick.rudigier@gantner.com>
2024-01-19 07:04:55 +0100

    added azure artifacts and aws identity file

--- frontend/create-common-scripts.sh
+++ frontend/create-common-scripts.sh
@@ -9,14 +9,62 @@ case $yn in
 esac
 
 if [ $gitIdentity ]; then
-        echo "Writing git identity file..."
         read -p "Enter your email address: " email
         read -p "Enter your name: " username
 
-        rm -rf ~/scripts/set-gantner-git-identity-file.sh
-        echo "#!/bin/sh" >> ~/scripts/set-gantner-git-identity-file.sh
-        echo "" >> ~/scripts/set-gantner-git-identity-file.sh
-        echo "git config --global user.email \"$email\"" >> ~/scripts/set-gantner-git-identity-file.sh
-        echo "git config --global user.name \"$username\"" >> ~/scripts/set-gantner-git-identity-file.sh
-        echo "echo \"Git identity (Gantner) set!\"" >> ~/scripts/set-gantner-git-identity-file.sh
+        fileName=~/scripts/set-gantner-git-identity.sh
+        rm -rf $fileName
+        echo "#!/bin/sh" >> $fileName
+        echo "" >> $fileName
+        echo "git config --global user.email \"$email\"" >> $fileName
+        echo "git config --global user.name \"$username\"" >> $fileName
+        echo "echo \"Git identity (Gantner) set!\"" >> $fileName
+        echo "Creating git identity file..."
+fi
+
+read -p "Do you want to to create azure artifacts identity file? (yes/no) " yn
+
+case $yn in
+        yes ) aaIdentity=1;;
+        no ) echo Will not create azure artifacts identity file;;
+        * ) echo Will not create azure artifacts identity file;;
+esac
+
+if [ $aaIdentity ]; then
+        read -p "Enter your email address: " email
+        read -p "Enter your access token: " token
+
+        fileName=~/scripts/set-azure-artifacts-npm-identity.sh
+        rm -rf $fileName
+        echo "#!/bin/sh" >> $fileName
+        echo "" >> $fileName
+        echo "export NPM_EMAIL=$email" >> $fileName
+        echo "export NPM_TOKEN=$token" >> $fileName
+        echo "export NPM_TOKEN_BASE64=$(echo -n $token | base64)" >> $fileName
+        echo "echo \"Azure Artifacts identity set!\"" >> $fileName
+        echo "Creating azure artifacts identity file..."
+fi
+
+read -p "Do you want to to create aws identity file? (yes/no) " yn
+
+case $yn in
+        yes ) awsIdentity=1;;
+        no ) echo Will not create aws identity file;;
+        * ) echo Will not create aws identity file;;
+esac
+
+if [ $awsIdentity ]; then
+        read -p "Enter your aws access key id: " accessKeyId
+        read -p "Enter your aws secret access key: " secretAccessKey
+        read -p "Enter your aws session token: " sessionToken
+
+        fileName=~/scripts/set-aws-identity.sh
+        rm -rf $fileName
+        echo "#!/bin/sh" >> $fileName
+        echo "" >> $fileName
+        echo "export AWS_ACCESS_KEY_ID=$accessKeyId" >> $fileName
+        echo "export AWS_SECRET_ACCESS_KEY=$secretAccessKey" >> $fileName
+        echo "export AWS_SESSION_TOKEN=$sessionToken" >> $fileName
+        echo "echo \"AWS identity set!\"" >> $fileName
+        echo "Creating aws identity file..."
 fi
