334375f8f9cbbace1522c0b46de77e4116f0b4cf
Patrick Rudigier <patrick.rudigier@gantner.com>
2025-09-19 13:54:12 +0200

    refactored script to use global list as source

--- security/npm-dependency-checker/check-packages.sh
+++ security/npm-dependency-checker/check-packages.sh
@@ -2,21 +2,23 @@
 
 # Script uses npm list to find all packages and dependecy packages in a project
 # and checks if any of them are listed in compromised-packages.txt
+# You can run this script directly with following command:
+#        sh <(curl -s https://raw.githubusercontent.com/Gantner-Electronics/coding-guidelines/refs/heads/main/security/npm-dependency-checker/check-packages.sh)
 
-JSON_FILE="compromised-packages.txt"
+PACKAGES_TO_FIND_FILE=$(curl -s https://raw.githubusercontent.com/Gantner-Electronics/coding-guidelines/refs/heads/main/security/npm-dependency-checker/compromised-packages.txt)
 
-if [[ ! -f "$JSON_FILE" ]]; then
-	echo "Error: $JSON_FILE not found."
+if [[ -z "{$PACKAGES_TO_FIND_FILE}" ]]; then
+	echo "Error: $PACKAGES_TO_FIND_FILE not found."
 	exit 1
 fi
 
 INSTALLED_PACKAGES=$(npm ls -a)
 
 # Read each package and version from the JSON file
-cat $JSON_FILE | while read -r package; do
+echo $PACKAGES_TO_FIND_FILE | while read -r package; do
 	echo $INSTALLED_PACKAGES | grep -o "$package$"
 
 	if [[ "$?" -eq 0 ]]; then
-		echo "Compromised package found: $name@$version"
+		echo "Compromised package found: $package"
 	fi
 done
